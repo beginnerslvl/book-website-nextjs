618b3011a3e343373ccf080b1a3c9d22a417e5ad
Mr.Majboor <82969990+beginnerslvl@users.noreply.github.com>
2024-03-10 14:04:03 +0500

    Update programming.tsx

--- src/app/programming.tsx
+++ src/app/programming.tsx
@@ -1,42 +1,48 @@
-// src/app/programming.tsx
-import { books } from '@/constants/mockData';
-import BookCard from '@/components/BookCard';
-import Header from '@/components/Header';
-import SideBar from '@/components/SideBar';
-import { motion } from 'framer-motion';
-import styles from './page.module.css';
-
-export default function Programming() {
-  const programmingBooks = books.filter(book => book.title.toLowerCase().includes('programming'));
-
+"use client"
+import BookCard from '@/components/BookCard'
+import Header from '@/components/Header'
+import SideBar from '@/components/SideBar'
+import { books } from '@/constants/mockData'
+import { motion } from 'framer-motion'
+import styles from './page.module.css'
+export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <Header />
-        <div className={styles.containerStyle}>
-          <section className={styles.content}>
-            <SideBar />
-          </section>
-          <div className={styles.grouper}>
-            <h1 className={styles.title}>Programming Books</h1>
-            <ul className={styles.ulGroupStyle}>
-              {programmingBooks.map((book, i) => (
-                <motion.li
-                  key={i}
-                  whileHover={{ scale: 1.1 }}
-                  whileTap={{ scale: 0.9 }}
-                  transition={{ type: 'spring', damping: 50, mass: 0.75 }}
-                  initial={{ opacity: 0, x: 200 * (i + 1) }}
-                  animate={{ opacity: 1, x: 0 }}>
-                  <a href={`/book/${book.id}`} style={{ textDecoration: 'none' }}>
-                    <BookCard title={book.title} coverImage={book.image} description={book.description} />
-                  </a>
-                </motion.li>
-              ))}
-            </ul>
-          </div>
-        </div>
+  <div>
+
+    <Header/>
+
+      <div className={styles.containerStyle}>
+    <section className={styles.content}>
+<SideBar/>
+    </section>
+
+<div className={styles.grouper}>
+  <h1 className={styles.title}>maybe?</h1>
+  <ul className={styles.ulGroupStyle}>
+    {
+      books.map((book,i)=>
+        <motion.li 
+        whileHover={{scale:1.1}}
+        whileTap={{scale:0.9}}
+        transition={{type:'spring',damping:50,mass:0.75}}
+        initial={{opacity:0,x:200*(i+1)}}
+        animate={{opacity:1,x:0}}
+        key={i}>
+          <a href={`/book/${book.id}`} style={{textDecoration:'none'}} >
+           <BookCard title={book.title} coverImage={book.image} description={book.description} />
+          </a>
+        </motion.li>
+      )
+    }
+  </ul>
+</div>
+
       </div>
+
+  </div>
     </main>
-  );
+  )
 }
+
+
